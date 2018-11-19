cedd82413764ca16630056e2a8bd3da79da99b9d
nomadlife15u <aaeacd@gmail.com>
2018-11-19 23:31:43 +0900

    host name added

--- testproject/settings/base.py
+++ testproject/settings/base.py
@@ -24,7 +24,9 @@ SECRET_KEY = 'b^fv_)t39h%9p40)fnkfblo##jkr!$0)lkp6bpy!fi*f$4*92!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'nomadlife.pythonanywhere.com',
+]
 
 
 INSTALLED_APPS = [
